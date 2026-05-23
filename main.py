56aeb9f9c13c81f9cd9abfd6d47925af5fef0c38
marutaka1966 <marutaka1966@yahoo.co.jp>
2026-05-23 13:36:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -40,6 +40,5 @@ with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
     server.send_message(msg)
 
 print("メール送信完了")
-print(os.environ["print("メール送信完了")
 print(os.environ["GMAIL_USER"])
 raise Exception("STOP")
